c476b09417c2eb5ac2419281d57a802bdd364783
Arpit Pandey <ansh2011pandey@gmail.com>
2026-02-12 11:47:39 +0530

    Implement subset checking function in Problem19.py

---
+++ ClassWork Assignment/Problem19.py
@@ -0,0 +1,20 @@
+def is_subset(a, b):
+    counts = {}
+    
+    for num in a:
+        counts[num] = counts.get(num, 0) + 1
+        
+    for num in b:
+        if counts.get(num, 0) > 0:
+            counts[num] -= 1
+        else:
+            return False
+            
+    return True
+
+
+arr_a = [5, 1, 5, 8]
+arr_b = [5, 5, 1]
+
+result = is_subset(arr_a, arr_b)
+print(f"Is b a subset of a? {result}")
